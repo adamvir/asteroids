e65a8eb29d91d81618aba5e8ee5727d9b808994f
adamvir <virag.adam23@gmail.com>
2025-05-10 16:19:24 +0200

    player can move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,16 +8,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0, None, 0)
+        player.draw(screen)
         pygame.display.flip()
-    
+        player.update(dt)
+        clock.tick(60)
+        dt += clock.tick(60) / 1000
+        
+        
 
 if __name__ == "__main__":
     main()
